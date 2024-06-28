858ce6fe61a31866129e2a745a7121be3ec83d39
KevinDylanPrayudi <kevin.dylan.prayudi20@gmail.com>
2024-06-28 13:09:11 +0700

    refactor(bank_account): shrink into piece of code

--- banking_system.js
+++ banking_system.js
@@ -1,21 +1,35 @@
 import BankAccount from "./bank_account.js";
 import queries from "./utility/input/queries.js";
-import CustomError from "./utility/Error/CustomError.js";
-import depositIsValid from "./utility/validation/depositValidation.js";
-import typeOperationIsValid from "./utility/validation/typeOperationValidation.js";
+import validation from "./utility/validations/validation.js";
+import error400 from "./utility/error-handling/400.js"
 
 
 class BankingSystem extends BankAccount {
     constructor(balance) {
-        super(balance);
+        if (balance === null) return super(+balance)
+        try {
+            validation([
+                { errcode: 100, value: true, operator: "===", desiredValue: `isNaN(${+balance})`, message: "Please enter number instead of string" },
+                { errcode: 101, value: +balance, operator: "<", desiredValue: 1, message: "Please enter positive number" }
+            ])
+            super(+balance);
+        } catch (error) {
+            alert(error.message)
+            return new BankingSystem(queries("Enter starting balance"))
+        }
     }
 
     deposit(fn, typeOperation) {
         if (typeOperation !== "deposit") return
-        let amount = fn("Enter amount to deposit", 1)
+        let amount = fn("Enter amount to deposit")
+        console.log(`this is ${amount} of deposit`)
+        if (amount === null) return
         try {
-            depositIsValid(amount)
-            super.deposit(amount);
+            validation([
+                { errcode: 200, value: true, operator: "===", desiredValue: `isNaN(${+amount})`, message: "Please enter number instead of string" },
+                { errcode: 201, value: +amount, operator: "<", desiredValue: 1, message: "Please enter positive number" }
+            ])
+            super.deposit(+amount);
         } catch (err) {
             alert(err.message)
             return this.deposit(queries, typeOperation)
@@ -24,10 +38,14 @@ class BankingSystem extends BankAccount {
 
     withdraw(fn, typeOperation) {
         if (typeOperation !== "withdraw") return
-        let amount = fn("Enter amount to withdraw", 1)
+        let amount = fn("Enter amount to withdraw")
+        if (amount === null) return
         try {
-            depositIsValid(amount)
-            if (amount > this._balance) throw new CustomError(300, "You don't have enough money in your account. Your balance is " + this._balance)
+            validation([
+                { errcode: 300, value: true, operator: "===", desiredValue: `isNaN(${+amount})`, message: "Please enter number instead of string" },
+                { errcode: 301, value: +amount, operator: "<", desiredValue: 1, message: "Please enter positive number" },
+                { errcode: 302, value: +amount, operator: ">", desiredValue: this._balance, message: "You don't have enough money in your account. Your balance is " + this._balance }
+            ])
             super.withdraw(amount)
             return this._balance
         } catch (err) {
@@ -39,7 +57,7 @@ class BankingSystem extends BankAccount {
     getBalance(typeOperation) {
         if (typeOperation !== "balance") return;
         return new Promise((resolve) => setTimeout(() => {
-            resolve((super.getBalance() === undefined) ? 0 : super.getBalance())
+            resolve(alert(`Your balance is ${(super.getBalance() === undefined) ? 0 : super.getBalance()}.`))
         }, 1000))
     }
 }
@@ -47,37 +65,22 @@ class BankingSystem extends BankAccount {
 
 async function init(startingBalance, typeOperation) {
     let saldo = startingBalance;
-    let balance;
     try {
-        depositIsValid(saldo)
-        saldo = new BankingSystem(saldo)
-        typeOperation = typeOperationIsValid(await saldo.getBalance("balance"), typeOperation)
+        if (!(saldo instanceof BankingSystem)) saldo = new BankingSystem(saldo);
+        validation([
+            { errcode: 400, value: `${typeOperation == null} && ${saldo._balance === 0}`, operator: "===", desiredValue: true, message: "Please enter 'start', 'deposit', 'withdraw', or 'stop'. check validation" },
+            { errcode: 401, value: `${typeOperation == null} && ${saldo._balance !== 0}`, operator: "===", desiredValue: true, message: "Please enter 'deposit', 'withdraw', or 'stop'. check validation" }
+        ])
+        if (!typeOperation) typeOperation = queries("Enter another operation: deposit, withdraw, balance, or stop") || "stop"
         if (typeOperation === "stop") return
         saldo.deposit(queries, typeOperation)
         saldo.withdraw(queries, typeOperation)
-        balance = await saldo.getBalance(typeOperation)
-        if (balance !== undefined) {
-            alert(`Your balance is ${balance}.`)
-        }
-        return init(await saldo.getBalance("balance"), queries("Enter another operation: deposit, withdraw, balance, or stop"))
+        await saldo.getBalance(typeOperation)
+        return init(saldo, queries("Enter another operation: deposit, withdraw, balance, or stop")|| "stop")
     } catch (err) {
         alert(err.message)
-        if (err.code === 100) {
-            return init(queries("Enter starting balance", 1), "")
-        } else if (err.code === 201) {
-            typeOperation = queries("Enter another operation: deposit, withdraw, balance, or stop")
-            if (!typeOperation) return
-            return init(await saldo.getBalance("balance"), typeOperation)
-
-        } else if (err.code === 200) {
-            typeOperation = queries("Enter another operation: start deposit(start), deposit, withdraw, or stop")
-            if (typeOperation === "start") {
-                return init(queries("Enter starting balance", 1), "")
-            } else {
-                return alert(`Your balance is ${await saldo.getBalance("balance")}.`)
-            }
-        }
+        error400(err, saldo, init)
     }
 }
 
-init(queries("Enter starting balance", 1), "")
+init(queries("Enter starting balance"), null)
